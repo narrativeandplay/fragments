04781fc089eda78c169c202b18b8e5f8d3cf7543
Benedict Lee <benedictleejh@gmail.com>
2013-12-14 17:05:22 +0800

    Add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title(title)
+    application_name = Rails.application.class.parent_name.underscore.titleize
+
+    if title.empty?
+      application_name
+    else
+      "#{application_name} | #{title}"
+    end
+  end
 end
