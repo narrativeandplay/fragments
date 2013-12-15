3bdffdfc39d8942803f81d52958435c474208e00
Benedict Lee <benedictleejh@gmail.com>
2013-12-15 18:04:16 +0800

    Made username saving case sensitive, and login case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :username, presence: true, length: { minimum: 2 }
+  validates :username, presence: true, length: { minimum: 2 }, uniqueness: { case_sensitive: false }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if username = conditions.delete(:username)
+      where(conditions).where(["lower(username) = :value", { :value => username.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
