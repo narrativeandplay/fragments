d51059402fc19f73ebcfb26a9c98a80ff2c5d6e0
Benedict Lee <benedictleejh@gmail.com>
2013-12-31 20:41:48 +0800

    Added specs for stories and fragments controllers

---
+++ spec/controllers/stories_controller_spec.rb
@@ -0,0 +1,124 @@
+require 'spec_helper'
+
+describe StoriesController do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:story) { FactoryGirl.create(:story, creator: user) }
+  let!(:fragment) { FactoryGirl.create(:fragment, author:user, story: story) }
+  let!(:other_fragment) { FactoryGirl.create(:fragment, author:user, story: story, parent: fragment) }
+  
+  describe "GET #index" do
+    before { get :index}
+    
+    it 'populates an array of stories' do
+      assigns(:stories).should eq [story]
+    end
+    
+    it 'renders the index view' do
+      response.should render_template :index
+    end
+  end
+
+  describe "GET #new" do
+    before { get :new }
+    
+    it 'assigns a new Story to @story' do
+      assigns(:story).should be_a_new(Story)
+      assigns(:story).should_not be_changed
+    end
+    
+    it 'assigns a new Fragment to @fragment' do
+      assigns(:fragment).should be_a_new(Fragment)
+      assigns(:fragment).should_not be_changed
+    end
+    
+    it 'renders the :new template' do
+      response.should render_template :new
+    end
+  end
+
+  describe "GET #show" do
+    before { get :show, id: story }
+    
+    it 'assigns the requested story to @story' do
+      assigns(:story).should eq story
+    end
+    
+    it "assigns the story's fragments to @fragments" do
+      assigns(:fragments).should eq [fragment, other_fragment]
+    end
+    
+    it 'renders the :show template' do
+      response.should render_template :show
+    end
+  end
+
+  describe "POST #create" do
+    let(:story_attributes) { FactoryGirl.attributes_for(:story) }
+    let(:fragment_attributes) { FactoryGirl.attributes_for(:fragment, author: user, story: story_attributes) }
+    
+    before { sign_in user }
+    
+    context "with valid attributes" do
+      it 'creates a new story' do
+        expect {
+          post :create, story: story_attributes.merge(fragment: fragment_attributes)
+        }.to change(Story, :count).by(1)
+      end
+      
+      it 'creates a new fragment' do
+        expect {
+          post :create, story: story_attributes.merge(fragment: fragment_attributes)
+        }.to change(Fragment, :count).by(1)
+      end
+      
+      it 'redirects to the new story' do
+        post :create, story: story_attributes.merge(fragment: fragment_attributes)
+        response.should redirect_to Story.last
+      end
+    end
+
+    context "with invalid attributes" do
+      describe "blank title" do
+        let(:story_attributes) { FactoryGirl.attributes_for(:story, title: '  ') }
+
+        it 'does not save the new story' do
+          expect {
+            post :create, story: story_attributes.merge(fragment: fragment_attributes)
+          }.not_to change(Story, :count)
+        end
+
+        it 'does not save the new fragment' do
+          expect {
+            post :create, story: story_attributes.merge(fragment: fragment_attributes)
+          }.not_to change(Fragment, :count)
+        end
+
+        it 're-renders the new method' do
+          post :create, story: story_attributes.merge(fragment: fragment_attributes)
+          response.should render_template :new
+        end
+      end
+
+      describe "blank content" do
+        let(:fragment_attributes) { FactoryGirl.attributes_for(:fragment, content: '  ') }
+
+        it 'does not save the new story' do
+          expect {
+            post :create, story: story_attributes.merge(fragment: fragment_attributes)
+          }.not_to change(Story, :count)
+        end
+
+        it 'does not save the new fragment' do
+          expect {
+            post :create, story: story_attributes.merge(fragment: fragment_attributes)
+          }.not_to change(Fragment, :count)
+        end
+
+        it 're-renders the new method' do
+          post :create, story: story_attributes.merge(fragment: fragment_attributes)
+          response.should render_template :new
+        end
+      end
+    end
+  end
+end
