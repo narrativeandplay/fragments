1f4e543d6acf9406fdc7aeeb565cdbb5bf6f5306
Benedict Lee <benedictleejh@gmail.com>
2015-03-01 13:40:20 +0800

    Get comments in reverse chronological order

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :story
 
+  default_scope -> { order(created_at: :desc) }
+
   validates :user, presence: true
   validates :story, presence: true
   validates :text, presence: true
